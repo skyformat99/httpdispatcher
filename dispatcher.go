8763fa9f5b074494d289f9e11e22284bd6534ffb
dxvgef <dxvgef@outlook.com>
2018-02-07 06:54:00 +0800

    update

--- dispatcher.go
+++ dispatcher.go
@@ -7,31 +7,33 @@ import (
 )
 
 type Dispatcher struct {
-	httpRouter *httprouter.Router
-	Router     *RouterGroup
-	Handler    struct {
-		NotFound         Handler      //404错误处理器
-		MethodNotAllowed Handler      //405错误处理器
-		ServerError      Handler      //500错误处理器
-		Event            EventHandler //事件处理器
-	}
 	EventConfig struct {
-		EnableCaller     bool //启用来源记录
+		EnableCaller     bool //启用来源记录(影响性能)
 		ShortCaller      bool //缩短来源记录(仅记录源码文件名)
 		NotFound         bool //记录404错误事件
 		MethodNotAllowed bool //记录405错误事件
 		ServerError      bool //记录500错误事件
 	}
+	Router  *RouterGroup
+	Handler struct {
+		NotFound         Handler      //404错误处理器
+		MethodNotAllowed Handler      //405错误处理器
+		ServerError      Handler      //500错误处理器
+		Event            EventHandler //事件处理器
+	}
+	httpRouter *httprouter.Router
 }
 
 func New() *Dispatcher {
 	var d Dispatcher
 	//实例化httprouter路由
 	d.httpRouter = httprouter.New()
-	//指定各种错误处理
-	d.httpRouter.PanicHandler = d.serverErrorHandle                            //panic错误处理器
+	//指定http router的错误处理器
+	d.httpRouter.PanicHandler = d.panicErrorHandle                             //panic错误处理器
 	d.httpRouter.NotFound = http.HandlerFunc(d.notFoundHandle)                 //404错误处理器
 	d.httpRouter.MethodNotAllowed = http.HandlerFunc(d.methodNotAllowedHandle) //405错误处理器
+
+	//初始化路由
 	d.Router = &RouterGroup{
 		d: &d,
 	}
